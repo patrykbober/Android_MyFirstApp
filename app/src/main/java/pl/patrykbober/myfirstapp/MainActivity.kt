a85aa1878e98266306a46a2c89ff1f4d22597554
Patryk Bober <boberp26@gmail.com>
2020-04-08 18:16:56 +0200

    3-start-another-activity

--- app/src/main/java/pl/patrykbober/myfirstapp/MainActivity.kt
+++ app/src/main/java/pl/patrykbober/myfirstapp/MainActivity.kt
@@ -1,12 +1,28 @@
 package pl.patrykbober.myfirstapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+const val EXTRA_MESSAGE = "pl.patrykbober.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val editText3 = findViewById<EditText>(R.id.editText3)
+        val message = editText3.text.toString()
+        println(message)
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
